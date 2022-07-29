6935c9bce7dd8d24c03bddea0d05d0825ce8f80e
Assel Uspanova <esim2306@gmail.com>
2022-07-28 20:33:44 -0400

    SAdd come change to data faker

--- src/test/java/tiger/api/test/fake/DataGenerator.java
+++ src/test/java/tiger/api/test/fake/DataGenerator.java
@@ -12,6 +12,7 @@ public class DataGenerator{
 		return faker.name().firstName() + faker.name().lastName() + "@gmail.com";
 	}
 	
+	// ndgjhdjhkjgkflg
 	public static String getPhoneNumber() {
 		Faker faker = new Faker();
 		return faker.phoneNumber().cellPhone();
